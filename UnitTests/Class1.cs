75e6b266080cde435cb4bd10530e44ce4adb9650
Olle Paulsson <olle.paulsson@gyldendal.no>
2024-10-14 13:44:03 +0200

    Init checkin

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests;
+
+[TestClass]
+public class Class1
+{
+    public void TestMethod1()
+    {
+        // Arrange
+        var x = 1;
+        var y = 2;
+        var expected = 3;
+
+        // Act
+        var actual = x + y;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
